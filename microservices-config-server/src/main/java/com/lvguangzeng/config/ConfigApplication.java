b95d1ca5aac51559939c6c99a0b649ed7e1eeaaf
LvGuangzeng <995342528@qq.com>
2018-11-24 21:56:51 +0800

    eureka & config

---
+++ microservices-config-server/src/main/java/com/lvguangzeng/config/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.lvguangzeng.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+}
---
+++ microservices-eureka-server/src/main/java/com/lvguangzeng/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.lvguangzeng.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
